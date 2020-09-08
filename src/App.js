199eaff18d326e572a3b180ffb849b53574b5ff2
vedymin <michalskimlm@gmail.com>
2020-09-08 12:48:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// TODO
+//
+// 1. Add FORM with following fields and submit button
+// - title
+// - message
+// 2. Below the form list all items in databae with delete button next to each item.
+// 3. When click on submit, send data to database (POST) and refresh list.
+// 4. When click delete button, remove entry from DB and refresh list.
+
+import React from "react";
+import "./styles.css";
+
+export default () => {
+  return (
+    <div className="App">
+      <h1>Task 3</h1>
+      <h2>React</h2>
+    </div>
+  );
+};
